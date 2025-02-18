32dd5ed9cb3e07cb28add3c012a7bddde117cbd6
GiovannaSantoli <giihsantoli@gmail.com>
2025-02-18 12:34:37 -0300

    ex sist multas

---
+++ Aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 69
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if ( vel >  60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!!!`)}
+console.log(`Dirija sempre usando cinto de segurança!`)
